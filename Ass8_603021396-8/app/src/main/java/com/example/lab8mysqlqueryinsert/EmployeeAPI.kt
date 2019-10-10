ce0bebf57e3b1b6611d514914994f0deb14e7d10
lukka-na <lukkana.p@kkumail.com>
2019-10-10 13:40:13 +0700

    Ass8_603021396-8

---
+++ Ass8_603021396-8/app/src/main/java/com/example/lab8mysqlqueryinsert/EmployeeAPI.kt
@@ -0,0 +1,23 @@
+package com.example.lass8_3968
+
+
+
+import com.example.ass8_3968.Employee
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface EmployeeAPI {
+    @GET("allemp")
+    fun retriveEmp(): Call<List<Employee>>
+
+    @FormUrlEncoded
+    @POST("emp")
+    fun insertEmp(
+        @Field("emp_name") emp_name: String,
+        @Field("emp_gender") emp_gender: String,
+        @Field("emp_email") emp_email: String,
+        @Field("emp_salary") emp_salary: Int):Call<Employee>
+}
